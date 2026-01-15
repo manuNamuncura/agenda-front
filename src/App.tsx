87c9985c87bd0be4408b3798eb1be7e97ff71d8e
manuNamuncura <manu.namun94@gmail.com>
2026-01-15 09:48:12 -0300

    feat: configuracion de las rutas de login y dashboard

---
+++ src/App.tsx
@@ -0,0 +1,56 @@
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router';
+import { Toaster } from 'react-hot-toast'; // 1. Importamos el Toaster
+import './App.css';
+import { useAuthStore } from './features/auth/store/useAuthStore';
+import LoginPage from './features/auth/LoginPage';
+import DashboardPage from './features/matches/DashboardPage';
+
+function App() {
+  const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
+
+  return (
+    <BrowserRouter>
+      <Toaster 
+        position="top-right"
+        reverseOrder={false}
+        toastOptions={{
+          style: {
+            background: '#1a1a1a', 
+            color: '#fff',         
+            border: '1px solid rgba(255,255,255,0.1)',
+            borderRadius: '16px',   
+            fontSize: '14px',
+          },
+          success: {
+            iconTheme: {
+              primary: '#22c55e',
+              secondary: '#fff',
+            },
+          },
+        }}
+      />
+
+      <Routes>
+        {/* Ruta Pública */}
+        <Route
+          path='/login'
+          element={!isAuthenticated ? <LoginPage/> : <Navigate to="/dashboard" replace/>}
+        />
+
+        {/* Ruta Privada: Dashboard */}
+        <Route
+          path='/dashboard'
+          element={isAuthenticated ? <DashboardPage /> : <Navigate to="/login" replace />}
+        />
+
+        {/* Redirección inicial */}
+        <Route
+          path='/'
+          element={<Navigate to={isAuthenticated ? "/dashboard" : "/login"} replace />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
